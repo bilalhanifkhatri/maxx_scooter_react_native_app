d0feafbc34c3c9830e8bee6f499f9809840cb2c7
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2024-01-02 12:37:00 +0500

    update for deployment

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import HomeStackNavigator from "./src/navigator/Navigation";
 
+// eas build -p android --profile preview
 export default function App() {
   return (
     <NavigationContainer>
